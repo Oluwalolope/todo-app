d5eb9cf8fe45698f5844ad3ca19f44d6b8082207
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2024-04-17 18:00:58 +0100

    Added a few variables to the stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
 
 //Sass Task
 function scssTask() {
@@ -20,31 +19,14 @@ function jsTask() {
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
-//Browsersync Task
-function browsersyncServe(cb) {
-    browsersync.init({
-        server: {
-            baseDir: '.'
-        }
-    });
-    cb();
-}
-
-function browsersyncReload(cb) {
-    browsersync.reload();
-    cb();
-}
-
 //Watch Task
 function watchTask(){
-    watch('*.html', browsersyncReload);
-    watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+    watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask));
 }
 
 //Default Gulp task
 exports.default = series(
     scssTask,
     jsTask,
-    browsersyncServe,
     watchTask
 );
