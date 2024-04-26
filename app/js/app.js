5d531855dfd50a53d1b2ca5824191e9510fde7b9
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2024-04-26 19:49:07 +0100

    Succesfully deleted todos from local storage

--- app/js/app.js
+++ app/js/app.js
@@ -9,57 +9,60 @@ const desktopFilter = document.querySelector("form.filter--desktop--todo");
 const mobileFilter = document.querySelector("form.filter--mobile--todo");
 
 //Getting a reference for the clear completed todo button
-const clearCompletedTodosButton = document.querySelector('button.clear--completed');
-
-
+const clearCompletedTodosButton = document.querySelector("button.clear--completed");
 
 //Function for knowing the value of the filter chosen
 let filter = null;
 const filterValues = () => {
   //If the width of the screen is equal to or greater than 500 use the desktop filter, else use mobile filter
-  innerWidth >= 500 ? filter = desktopFilter.filter.value : filter = mobileFilter.filter.value;
+  innerWidth >= 500
+    ? (filter = desktopFilter.filter.value)
+    : (filter = mobileFilter.filter.value);
   return filter;
-}
-
+};
 
 //Function for checking the todo status
 const todoStatus = () => {
   Array.from(todoList.children)
-    .filter(todo => todo.classList.contains('todo'))
-      .forEach(todo => {
-        let checkBox = todo.querySelector('input[type=checkbox]');
-        //iterate through each todo and check wheter they have been checked or not
-        checkBox.checked ? 
-        todo.classList.add('completed') 
-        : 
-        todo.classList.remove('completed');
-      });
-}
+    .filter((todo) => todo.classList.contains("todo"))
+    .forEach((todo) => {
+      let checkBox = todo.querySelector("input[type=checkbox]");
+      //iterate through each todo and check wheter they have been checked or not
+      checkBox.checked
+        ? todo.classList.add("completed")
+        : todo.classList.remove("completed");
 
-//Function for counting the todos
+      todo.classList.contains("completed")
+        ? updateTodo(todo.getAttribute("id"))
+        : console.log("could not get the id");
+    });
+};
+
 const todoCount = () => {
-  let todoCounter = document.querySelector('.todo--counter > span');
-  
+  //Function for counting the todos
+  let todoCounter = document.querySelector(".todo--counter > span");
+
   todoCounter.textContent =
-  //Only count todos that are visibly rendered on the screen
-  Array.from(todoList.children)
-  .filter(todo => todo.classList.contains("todo"))
-  .filter(todo => todo.style.display !== "none").length;
-}
+    //Only count todos that are visibly rendered on the screen
+    Array.from(todoList.children)
+      .filter((todo) => todo.classList.contains("todo"))
+      .filter((todo) => todo.style.display !== "none").length;
+};
 
 //Function for getting todos from local storage
-const getTodos = () => JSON.parse(localStorage.getItem('todos')) || [];
+const getTodos = () => JSON.parse(localStorage.getItem("todos")) || [];
 
 //Function for storing todos in local storage
 const saveTodos = (todos) => {
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 //Todo Template Function
-const generateTemplate = (todo) => {
+const generateTemplate = (todo, id) => {
   const newTodo = document.createElement("div");
   newTodo.classList.add("input--wrapper");
   newTodo.classList.add("todo");
+  newTodo.setAttribute("id", id);
   newTodo.innerHTML = `
      <div class="list--wrapper">
         <input type="checkbox" name="checkbox" class="check--todo">
@@ -80,6 +83,7 @@ const renderTodosFromLocalStorage = () => {
     const newTodo = document.createElement("div");
     newTodo.classList.add("input--wrapper");
     newTodo.classList.add("todo");
+    newTodo.setAttribute("id", storedTodo.id);
     newTodo.innerHTML = `
        <div class="list--wrapper">
           <input type="checkbox" name="checkbox" class="check--todo">
@@ -87,13 +91,32 @@ const renderTodosFromLocalStorage = () => {
         </div>
        <button class="delete--todo">
          <img src="assets/images/icon-cross.svg" alt="cross icon">
-       </button>
-    `;
+         </button>
+         `;
+    const checkBox = newTodo.querySelector("input[type=checkbox]");
+    checkBox.checked = storedTodo.complete;
     todoList.prepend(newTodo);
-
   });
 
   todoCount();
+};
+
+
+//function for updating todo status in local storage
+const updateTodo = (todoId) => {
+  console.log(todoId);
+};
+
+//function to delete todos from local storage
+const deleteTodo = (todoId) => {
+  //Retrieve existing todos
+  let todos = getTodos();
+
+  //Filter out the todo with the specified Id
+  todos = todos.filter(todo => todo.id != todoId);
+
+  //Update local storage
+  saveTodos(todos);
 }
 
 //Add new todos
@@ -106,90 +129,95 @@ addForm.addEventListener("submit", (e) => {
     todos.push({
       id: todoId,
       todo: todoText,
-      complete: true
+      complete: false,
     });
     saveTodos(todos);
-    generateTemplate(todoText);
-    todoCount();//update the todo count
-    addForm.reset();//Reset the input after submission
-
-    // generateTemplate(todo); //only add todos that are atleast a character long
-    // todoCount();//update the todo count
-    // addForm.reset(); //Reset the input after submission
+    generateTemplate(todoText, todoId);
+    todoCount(); //update the todo count
+    addForm.reset(); //Reset the input after submission
   }
 });
 
 //Remove Todos
 todoList.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete--todo")) {
-    e.target.parentElement.remove();//if the button is clicked
-  } else if (e.target.parentElement.classList.contains("delete--todo")) {
-    e.target.parentElement.parentElement.remove();//if the image icon wrapped inside the button is clicked
+    let deletedId = e.target.parentElement.getAttribute("id"); 
+    deleteTodo(deletedId); //delete the todo from local storage
+    e.target.parentElement.remove(); //if the button is clicked
+  } 
+  else if (e.target.parentElement.classList.contains("delete--todo")) {
+    let deletedId = e.target.parentElement.parentElement.getAttribute("id"); 
+    deleteTodo(deletedId); //delete the todo from local storage
+    e.target.parentElement.parentElement.remove(); //if the image icon wrapped inside the button is clicked
   }
-  todoCount();//update todo count
+  todoCount(); //update todo count
 });
 
 //Function for Clearing Completed Todos
 const clearCompletedTodos = () => {
-  todoStatus();//check whether the todos have been checked
+  todoStatus(); //check whether the todos have been checked
 
   Array.from(todoList.children)
-  .filter(todo => todo.classList.contains('todo'))
-    .forEach(todo => {
-      if(todo.classList.contains('completed')) {
-        todo.remove();
-      };
-  });
-
-  todoCount();//update the todo count
-}
+    .filter((todo) => todo.classList.contains("todo"))
+    .forEach((todo) => {
+      if (todo.classList.contains("completed")) {
+        deleteTodo(todo.getAttribute('id'));//remove the todos from local storage
+        todo.remove();//remove the todos from the rendered ui
+      }
+    });
+  todoCount(); //update the todo count
+};
 
 //function for filtering todos
 const filterTodos = (filter) => {
-  if(filter == "all"){
+  if (filter == "all") {
     Array.from(todoList.children)
-    .filter(todo => todo.classList.contains('todo'))
-      .forEach(todo => {
+      .filter((todo) => todo.classList.contains("todo"))
+      .forEach((todo) => {
         todo.style.display = "flex";
       });
     todoCount();
   }
-  if(filter == "active"){
+  if (filter == "active") {
     Array.from(todoList.children)
-    .filter(todo => todo.classList.contains('todo'))
-      .forEach(todo => {
-        todo.classList.contains('completed')? todo.style.display = "none" : todo.style.display = "flex";
+      .filter((todo) => todo.classList.contains("todo"))
+      .forEach((todo) => {
+        todo.classList.contains("completed")
+          ? (todo.style.display = "none")
+          : (todo.style.display = "flex");
       });
     todoCount();
   }
-  if(filter == "completed"){
+  if (filter == "completed") {
     Array.from(todoList.children)
-    .filter(todo => todo.classList.contains('todo'))
-      .forEach(todo => {
-        todo.classList.contains('completed')? todo.style.display = "flex" : todo.style.display= "none";
+      .filter((todo) => todo.classList.contains("todo"))
+      .forEach((todo) => {
+        todo.classList.contains("completed")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none");
       });
     todoCount();
   }
-}
+};
 
 //Listen for when a filter button is clicked on desktop mode
-desktopFilter.addEventListener('click', e => {
+desktopFilter.addEventListener("click", (e) => {
   todoStatus();
   filterValues();
   filterTodos(filter);
 });
 
 //Listen for when a filter button is clicked on mobile mode
-mobileFilter.addEventListener('click', e => {
+mobileFilter.addEventListener("click", (e) => {
   todoStatus();
   filterValues();
   filterTodos(filter);
 });
 
-//Listen for when the clear completed todos button is clicked 
-clearCompletedTodosButton.addEventListener('click', () => {
+//Listen for when the clear completed todos button is clicked
+clearCompletedTodosButton.addEventListener("click", () => {
   clearCompletedTodos();
 });
 
-todoCount();//count the todos on page load
-renderTodosFromLocalStorage();//Render todos from local storage on page load
+todoCount(); //count the todos on page load
+renderTodosFromLocalStorage(); //Render todos from local storage on page load
