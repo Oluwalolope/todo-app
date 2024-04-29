c7de01cb87c9bfd2aff526a634854cb5bb7253b8
Oluwalolope Adeleye <lopeadeleye@gmail.com>
2024-04-29 01:52:58 +0100

    Update toggle.js
    Updated the File path for the background images

--- app/js/toggle.js
+++ app/js/toggle.js
@@ -23,7 +23,7 @@ const enableDarkMode = () => {
     darkModeToggle.querySelector("img").setAttribute("src", "assets/images/icon-sun.svg");
 
     //change the background image
-    document.body.style.backgroundImage = "url(../assets/images/bg-desktop-dark.jpg)";
+    document.body.style.backgroundImage = "url(assets/images/bg-desktop-dark.jpg)";
 }
 
 const disableDarkMode = () => {
@@ -42,7 +42,7 @@ const disableDarkMode = () => {
     darkModeToggle.querySelector('img').setAttribute('src', 'assets/images/icon-moon.svg');
 
     //change the background image
-    document.body.style.backgroundImage = "url(../assets/images/bg-desktop-light.jpg)";
+    document.body.style.backgroundImage = "url(assets/images/bg-desktop-light.jpg)";
 }
 
 const colorModeFromLocalStorage = () => {
